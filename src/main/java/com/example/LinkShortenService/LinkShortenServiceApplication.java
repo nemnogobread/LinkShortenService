2dd7b081fe85374bd96bedebb9b817b5fa95bde5
Gleb <glebmiloslavov@gmail.com>
2025-11-28 21:31:26 +0300

    init commit

---
+++ src/main/java/com/example/LinkShortenService/LinkShortenServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.LinkShortenService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkShortenServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkShortenServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/LinkShortenService/LinkShortenServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LinkShortenService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkShortenServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
